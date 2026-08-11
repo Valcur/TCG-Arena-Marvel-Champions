3eca86e38cef5ad642adb7c1108f70c3386a459f
Loic <>
2026-08-11 20:28:24 +0200

    no message

--- functions.jsx
+++ functions.jsx
@@ -11,9 +11,10 @@ async function playVilainDeck() {
   const card = getVillainDeckTopCard()
   if (!card) return
   const cardData = await functions.getCardData(card)
+  console.log(cardData)
   game.data.GameplayManager.boost = cardData.boost
+  game.data.GameplayManager.star = cardData.starIcon
   if (cardData.starIcon) {
-    game.data.GameplayManager.star = true
     // create trigger effect to stack ? unowned stack effect a voir
   }
   await functions.moveCard(card, "Revealed")
