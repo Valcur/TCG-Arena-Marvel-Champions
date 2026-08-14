9fc097ca02d9a91676591d0dabec98fff428830e
Loic <>
2026-08-14 21:19:25 +0200

    no message

--- functions.jsx
+++ functions.jsx
@@ -3,7 +3,15 @@ function getVillainDeckTopCard() {
   //console.log(cards)
   //console.log("EncounterDeck:", cards?.EncounterDeck)
   const deck = cards?.EncounterDeck
-  if (!deck || deck?.length === 0) return null
+  if (!deck || deck?.length === 0) {
+    // shuffle revealed and discard
+    cards?.EncounterDeck = [...cards?.EncounterDiscard, ...cards?.Revealed]
+    cards?.Revealed = []
+    cards?.EncounterDiscard = []
+    await functions.shuffleSection("EncounterDeck");
+    await functions.repositionCards();
+    return null
+  }
   return cards.EncounterDeck[cards.EncounterDeck.length - 1]
 }
 
@@ -29,6 +37,9 @@ async function dealEncounter() {
     destination = "VillainAttachments"
   } else if (type === "Side Scheme") {
     destination = "SideSchemes"
+    card.counters[0] = {
+      value: 2
+    }
   }
 
   await functions.moveCard(card, destination)
@@ -51,6 +62,19 @@ function displayedEngagedEnemiesValue() {
     }
   }
 }
+
+/*
+NEMESIS SIDE SCHEMES
+If a card instructs a player to put a nemesis side scheme
+into play, the nemesis side scheme associated with the
+player that drew the card is placed in the play area, and
+their nemesis minion is put into play engaged with that
+player. The other encounter cards associated with that
+nemesis are placed in the encounter discard pile.
+Each hero’s nemesis, side scheme, and associated cards
+
+-> a voir si on automatise
+*/
 
 // onCardEnter -> movecard, destintaion == ma section && destintaion !== original
 function addedEngagedEnnemies(card) {
@@ -89,7 +113,9 @@ function turnEnd() {
 
   -> si je suis alterego: j'affiche le total scheme des minions sur moi, attque si je suis en héro
 
-  Pass the first player token to the next clockwise player and end the round.*/
+  Pass the first player token to the next clockwise player and end the round.
+  -> joueur actif clic sur fin de tour, on vide revealed
+  */
 }
 
 async function spawnDeck(decklist) {
@@ -175,30 +201,27 @@ Rencontre de cette Némésis.
 
 6. ✅Mélanger les decks des joueurs. Chaque joueur mélange son deck Joueur.
 
-8. Choisir un méchant. Choisissez un méchant et
+8. ✅Choisir un méchant. Choisissez un méchant et
 mettez en jeu son deck Méchant ainsi que son deck
 Manigance Principale au centre de la zone de jeu.
--> Le joueur qui appuie sur play est le seul a avoir accès a la sélection du deck commun
 
 9. ✅Déterminer les points de vie du méchant. Réglez
 le compteur de points de vie du méchant à la valeur
 indiquée sur sa carte Méchant.
 
-10. Résoudre la mise en place de la manigance. Résolvez
+10. ✅Résoudre la mise en place de la manigance. Résolvez
 toute instruction « Mise en place » sur la face 1A de
 la carte Manigance Principale. Résolvez toute capacité « Une fois révélée » sur les cartes Rencontre qui
 entrent en jeu lors de la mise en place.
 
-11. Mélanger le deck Rencontre. Mélangez dans le deck
+11. ✅Mélanger le deck Rencontre. Mélangez dans le deck
 Rencontre du méchant les cartes Obligation mises de
 côté lors de l’étape 4.
--> foreach -> movecard obligation section jsuque dans mechant deck
 
-12. Piocher des cartes. Chaque joueur pioche des cartes
+12. ✅Piocher des cartes. Chaque joueur pioche des cartes
 jusqu’à avoir en main un nombre de cartes égal à la
 valeur de sa taille de main indiquée en bas de sa carte
 Identité.
--> for reach + draw X de cardData.X
 
 13. ✅Résoudre les mulligans. Chaque joueur peut défausser n’importe quel nombre de cartes de sa main et
 piocher des cartes jusqu’à atteindre sa taille de main.
@@ -210,7 +233,6 @@ les cartes ainsi défaussées.)
 inscrite sur les cartes Identité en jeu.
 -> A faire manuellement
 */
-
 
 const villain = {
   "rhino": {
