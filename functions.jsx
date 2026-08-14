5da173dc1878fc5c47e07e1a1d7fb3f85d9fb9da
Loic <>
2026-08-14 14:53:02 +0200

    no message

--- functions.jsx
+++ functions.jsx
@@ -141,11 +141,13 @@ gamedata.villain.lifepoints = cardData.startingLifepoints*/
 async function allPlayerReady() {
   if (game.isHost) {
     await iniVilainDeck()
+  } else {
+    await iniVilainDeck()
   }
   const identityCard = cards?.Identity?.[0]
   if (!identityCard) return
   const cardData = await functions.getCardData(identityCard)
-  console.log(cardData)
+  console.log(game)
   if (!cardData) return
   await functions.draw(6)
   await functions.changeCounterValue(0, 2)
