6fff1c3cfd8df76984d3864ed1b1d58a2e673360
Loic <>
2026-08-19 14:00:12 +0200

    no message

--- rb-functions.jsx
+++ rb-functions.jsx
@@ -1,8 +1,10 @@
 async function placeBattlefields() {
+  console.log("yo", game, cards)
   if (!game.isHost) return
   if (!cards?.Battlefields?.length > 0) {
     for (const card of cards?.Battlefields) {
       if (card.owner === game.playerId) {
+          console.log("yo success")
         await functions.moveCard(card, "myBF2")
       }
     }
