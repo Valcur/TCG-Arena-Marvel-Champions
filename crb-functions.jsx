2768a0c22dd5c09b7c9394ef3ff04c1a16b442fc
Loic <>
2026-08-19 17:14:26 +0200

    no message

--- crb-functions.jsx
+++ crb-functions.jsx
@@ -3,14 +3,14 @@ async function drawIfNewCookie() {
   const cookie2 = cards?.Cookie2?.length > 0 ? cards.Cookie2[0]?.id : null
   const prevCookie1 = game.data.Scripts.lastCookie1
   const prevCookie2 = game.data.Scripts.lastCookie2
-  console.log(cards, game?.data.Scripts, functions)
-  if (cookie1 !== prevCookie1) {
+  console.log(cards, game?.data.Scripts)
+  if (cookie1 && cookie1 !== prevCookie1) {
     const cookie1Data = functions.getCardData(cookie1)
     await functions.draw(cookie1Data.HP ?? 2, false, "HP1")
     game.data.Scripts.lastCookie1 = cookie1.id
   }
-  if (cookie2 !== prevCookie2) {
-      const cookie2Data = functions.getCardData(cookie1)
+  if (cookie2 && cookie2 !== prevCookie2) {
+    const cookie2Data = functions.getCardData(cookie1)
     await functions.draw(cookie2Data.HP ?? 2, false, "HP2")
     game.data.Scripts.lastCookie2 = cookie2.id
   }
