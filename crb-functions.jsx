0147a4b9259915c0fc396d961a3abfaac5e2e749
Loic <>
2026-08-24 12:15:25 +0200

    no message

--- crb-functions.jsx
+++ crb-functions.jsx
@@ -4,19 +4,23 @@ async function drawIfNewCookie() {
   const prevCookie1 = game.data.Scripts.lastCookie1
   const prevCookie2 = game.data.Scripts.lastCookie2
 
-  if (cookie1 && cookie1.id !== prevCookie1 && (cards?.HP1?.length ?? 0) === 0) {
-    const cookie1Data = functions.getCardData(cookie1)
-    //const hp1ToDraw = cookie1Data?.hp ?? 0
-    const hp1ToDraw = cookie1Data?.face?.front?.hp ?? 2
-    await functions.draw(hp1ToDraw, false, "HP1")
+  if (cookie1 && cookie1.id !== prevCookie1) {
     game.data.Scripts.lastCookie1 = cookie1.id
+    if ((cards?.HP1?.length ?? 0) === 0) {
+      const cookie1Data = functions.getCardData(cookie1)
+      //const hp1ToDraw = cookie1Data?.hp ?? 0
+      const hp1ToDraw = cookie1Data?.face?.front?.hp ?? 2
+      await functions.draw(hp1ToDraw, false, "HP1")
+    }
   }
 
-  if (cookie2 && cookie2.id !== prevCookie2 && (cards?.HP2?.length ?? 0) === 0) {
-    const cookie2Data = functions.getCardData(cookie2)
-    //const hp2ToDraw = cookie2Data?.hp ?? 0
-    const hp2ToDraw = cookie2Data?.face?.front?.hp ?? 2
-    await functions.draw(hp2ToDraw, false, "HP2")
+  if (cookie2 && cookie2.id !== prevCookie2) {
     game.data.Scripts.lastCookie2 = cookie2.id
+    if ((cards?.HP2?.length ?? 0) === 0) {
+      const cookie2Data = functions.getCardData(cookie2)
+      //const hp2ToDraw = cookie2Data?.hp ?? 0
+      const hp2ToDraw = cookie2Data?.face?.front?.hp ?? 2
+      await functions.draw(hp2ToDraw, false, "HP2")
+    }
   }
 }
